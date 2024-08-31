f20335566985310bf97d68dac40de31ca5ff5037
Igor Dvorkin <idvorkin@gmail.com>
2024-08-31 09:44:56 -0700

    Adding

--- assets/process_png.py
+++ assets/process_png.py
@@ -1,18 +1,22 @@
-#!python3
 import os
 
 # for every png file, if there is not a webp file, call convert on it, then move the source. to be .pold
 
-print ("converting")
+processed_count = 0
+ignored_count = 0
 
 for file in os.listdir("."):
     if not file.endswith(".png"):
+        ignored_count += 1
         continue
-    webp = file[:-4] + ".webp"
+    webp = file + ".webp"
     if os.path.exists(webp):
+        ignored_count += 1
         continue
-    print(file)
     os.system(f"convert {file} {webp}")
     os.system(f"mv {file} {file}.old")
     os.system(f"git add {webp}")
+    processed_count += 1
     print("done")
+
+print(f"Processed files: {processed_count}, Ignored files: {ignored_count}")
